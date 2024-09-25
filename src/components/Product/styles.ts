438a33f12a8919c6a252bd75c8a968d5451f1caf
Thiago <thiagofeldmann06@gmail.com>
2024-09-25 14:19:05 -0300

    Conclusão parte 1

---
+++ src/components/Product/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+import { TagContainer } from '../Tag/styles'
+
+export const Card = styled.div`
+  background-color: ${cores.cinza};
+  border-radius: 8px;
+  padding: 8px;
+  position: relative;
+
+  margin-right: 24px;
+
+  ${TagContainer} {
+    margin-right: 8px;
+  }
+`
+
+export const Titulo = styled.h3`
+  font-weight: bold;
+  font-size: 24px;
+  display: block;
+  margin: 16px 0 8px 0;
+`
+
+export const Descricao = styled.p`
+  font-size: 14px;
+  line-height: 22px;
+  display: block;
+  margin-top: 16px;
+`
+
+export const Infos = styled.div`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+`
---
+++ src/components/ProductList/index.tsx
@@ -0,0 +1,32 @@
+import Game from '../../models/Game'
+import Product from '../Product'
+import { Container, List, Title } from './styles'
+
+export type Props = {
+  title: string
+  background: 'gray' | 'black'
+  games: Game[]
+}
+
+const ProductList = ({ background, title, games }: Props) => (
+  <Container background={background}>
+    <div className="container">
+      <Title>{title}</Title>
+      <List>
+        {games.map((game) => (
+          <Product
+            key={game.id}
+            title={game.title}
+            category={game.category}
+            system={game.system}
+            description={game.description}
+            infos={game.infos}
+            image={game.image}
+          />
+        ))}
+      </List>
+    </div>
+  </Container>
+)
+
+export default ProductList
