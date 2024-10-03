d8687b4d0aef873d1847dfc81adfa04930a24910
Thiago <thiagofeldmann06@gmail.com>
2024-10-03 14:41:25 -0300

    conclusao parte 2

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,114 @@
+import { useState } from 'react'
+
+import Section from '../Section'
+import { GalleryItem } from '../../pages/Home'
+
+import { Items, Item, Action, Modal, ModalContent } from './styles'
+
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
+import zoom from '../../assets/images/zoom.png'
+import play from '../../assets/images/play.png'
+import fechar from '../../assets/images/fechar.png'
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ?si=l0wYaFdQ-GMndn-Y'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+  items: GalleryItem[]
+}
+
+interface ModalState extends GalleryItem {
+  isVisible: boolean
+}
+
+const Gallery = ({ defaultCover, name, items }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  const closeModal = () => {
+    setModal({
+      isVisible: false,
+      type: 'image',
+      url: ''
+    })
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="black">
+        <Items>
+          {items.map((media, index) => (
+            <Item
+              key={media.url}
+              onClick={() => {
+                setModal({
+                  isVisible: true,
+                  type: media.type,
+                  url: media.url
+                })
+              }}
+            >
+              <img
+                src={getMediaCover(media)}
+                alt={`Mídia ${index + 1} de ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximar a mídia"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal className={modal.isVisible ? 'visible' : ''}>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img
+              src={fechar}
+              alt="Ícone de fechar"
+              onClick={() => closeModal()}
+            />
+          </header>
+          {modal.type === 'image' ? (
+            <img src={modal.url} />
+          ) : (
+            <iframe frameBorder={0} src={modal.url} />
+          )}
+        </ModalContent>
+        <div onClick={() => closeModal()} className="overlay"></div>
+      </Modal>
+    </>
+  )
+}
+
+export default Gallery
--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,4 +1,4 @@
-import Game from '../../models/Game'
+import { Game } from '../../pages/Home'
 import Product from '../Product'
 import { Container, List, Title } from './styles'
 
@@ -8,25 +8,54 @@ export type Props = {
   games: Game[]
 }
 
-const ProductList = ({ background, title, games }: Props) => (
-  <Container background={background}>
-    <div className="container">
-      <Title>{title}</Title>
-      <List>
-        {games.map((game) => (
-          <Product
-            key={game.id}
-            title={game.title}
-            category={game.category}
-            system={game.system}
-            description={game.description}
-            infos={game.infos}
-            image={game.image}
-          />
-        ))}
-      </List>
-    </div>
-  </Container>
-)
+export const formataPreco = (preco = 0) => {
+  return new Intl.NumberFormat('pr-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(preco)
+}
+
+const ProductList = ({ background, title, games }: Props) => {
+  const getGameTags = (game: Game) => {
+    const tags = []
+
+    if (game.release_date) {
+      tags.push(game.release_date)
+    }
+
+    if (game.prices.discount) {
+      tags.push(`${game.prices.discount}%`)
+    }
+
+    if (game.prices.current) {
+      tags.push(formataPreco(game.prices.current))
+    }
+
+    return tags
+  }
+
+  return (
+    <Container background={background}>
+      <div className="container">
+        <Title>{title}</Title>
+        <List>
+          {games.map((game) => (
+            <li key={game.id}>
+              <Product
+                id={game.id}
+                title={game.name}
+                category={game.details.category}
+                system={game.details.system}
+                description={game.description}
+                infos={getGameTags(game)}
+                image={game.media.thumbnail}
+              />
+            </li>
+          ))}
+        </List>
+      </div>
+    </Container>
+  )
+}
 
 export default ProductList
